3e96b1381b1c8931c368271a3c3dbfd06142623e
petersinal <petko.sinal@gmail.com>
2022-01-18 21:51:06 +0100

    Changes by Oliver

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const {src, dest, watch, series} = require('gulp');
+const postcss = require('gulp-postcss');
+const uglifycss = require('gulp-uglifycss');
+
+function buildStyles() {
+    const tailwindcss = require('tailwindcss');
+    return src('./assets/*.css')
+        .pipe(postcss([
+            tailwindcss('./tailwind.config.js'),
+        ]))
+        .pipe(uglifycss())
+        .pipe(dest('./www/assets/styles/'));
+}
+
+function watchFiles() {
+    watch(['./assets/*.css', './app/**/*.{html,js,latte}'], buildStyles());
+    console.log("Watching for Changes..\n");
+}
+
+exports.default = series(
+    buildStyles,
+    watchFiles,
+);
+
